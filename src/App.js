26458bfb4c0cd351fcef74a0087c30e4d1b1dbf2
Stefan Evans <stefan.evans@consegna.cloud>
2022-04-15 09:27:22 +1200

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Counter from './components/Counter';
+
+
+import Todo from './components/Todo'
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo />
+      <Todo />
+      <Todo />           
+    </div>
+  );
+}
+
+export default App;
