6dafe15609cbe55af95b4c86daea8d0117aa5fe5
SE <stefan.evans@consegna.cloud>
2022-04-15 10:50:55 +1200

    added a prop

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo />
-      <Todo />
-      <Todo />           
+      <Todo text='Learn React'/>
+      <Todo text='Master React'/>
+      <Todo text='Teach React'/>           
     </div>
   );
 }
